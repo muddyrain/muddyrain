fb4e429e736af688da3db0a36d516cb5af0cd194
‘muddyrain’ <qiu3291002845@gmail.com>
2023-06-18 22:38:20 +0800

    feat(tailor): 修改图片展示改为canvas元素展示

--- src/tailor/demo/demo1.tsx
+++ src/tailor/demo/demo1.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react';
 import Tailor from '..';
-import jpg1 from '../images/1.png';
+import image1 from '../images/1.png';
+import image2 from '../images/2.png';
+import image3 from '../images/3.png';
 
 const Demo1: FC = () => {
-	return <Tailor src={jpg1}></Tailor>;
+	return (
+		<div className="flex">
+			<h2>小于容器尺寸</h2>
+			<Tailor src={image1}></Tailor>
+			<h2>大于宽度容器尺寸</h2>
+			<Tailor src={image2}></Tailor>
+			<h2>大于高度容器尺寸</h2>
+			<Tailor src={image3}></Tailor>
+		</div>
+	);
 };
 
 export default Demo1;
--- src/tailor/index.tsx
+++ src/tailor/index.tsx
@@ -1,5 +1,4 @@
-import React, { FC, useEffect, useRef, useState } from 'react';
-import Dialog from '../dialog';
+import React, { FC, useEffect, useRef } from 'react';
 import styles from './index.module.less';
 import type { TailorProps } from './types';
 
@@ -7,110 +6,70 @@ import type { TailorProps } from './types';
  * 裁剪
  */
 const Tailor: FC<TailorProps> = ({ src }) => {
-	const imgRef = useRef<HTMLImageElement>(null);
 	const dragRef = useRef<HTMLDivElement>(null);
-	const [visible, setVisible] = useState(true);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const wrapRef = useRef<HTMLDivElement>(null);
 
-	const [base64URL, setBase64URL] = useState('');
-	const imgObject = useRef(new Image());
-	const clipImage = (
-		left: number,
-		top: number,
-		width: number,
-		height: number
-	) => {
-		const imageElement = imgRef.current;
-		if (!imageElement) return;
-		const canvasElement = document.createElement('canvas') as HTMLCanvasElement;
-		canvasElement.width = width;
-		canvasElement.height = height;
-		const ctx = canvasElement.getContext('2d');
-		if (ctx) {
-			ctx.drawImage(
-				imgObject.current,
-				left,
-				top,
-				width,
-				height,
-				0,
-				0,
-				width,
-				height
-			);
-			const _base64URL = canvasElement.toDataURL('image/jpeg', 1);
-			setBase64URL(_base64URL);
+	const state = useRef<{
+		imageScale: number;
+	}>({
+		imageScale: 1,
+	});
+	/**
+	 * 初始化canvas
+	 */
+	const initCanvas = () => {
+		const canvasElement = canvasRef.current;
+		const wrapElement = wrapRef.current;
+		if (canvasElement && wrapElement) {
+			const ctx = canvasElement.getContext('2d');
+			const image = new Image();
+			image.src = src;
+			const { width: wrapWidth, height: wrapHeight } =
+				wrapElement.getBoundingClientRect();
+			const wrapProportion = wrapWidth / wrapHeight;
+
+			image.onload = () => {
+				const { width: imageWidth, height: imageHeight } = image;
+				const imageProportion = imageWidth / imageHeight;
+
+				// 如果图片尺寸小于容器尺寸
+				if (imageWidth <= wrapWidth && imageHeight <= wrapHeight) {
+					state.current.imageScale = 1;
+				} else if (imageProportion > wrapProportion) {
+					state.current.imageScale = wrapWidth / imageWidth;
+				} else {
+					state.current.imageScale = wrapHeight / imageHeight;
+				}
+				const scaleImgWidth = state.current.imageScale * imageWidth;
+				const scaleImgHeight = state.current.imageScale * imageHeight;
+
+				canvasElement.width = scaleImgWidth;
+				canvasElement.height = scaleImgHeight;
+				ctx?.drawImage(image, 0, 0, scaleImgWidth, scaleImgHeight);
+			};
 		}
 	};
-	const initImage = () => {
-		imgObject.current.src = src;
-		imgObject.current.setAttribute('crossOrigin', 'Anonymous');
-		imgObject.current.onload = () => {};
-	};
 	useEffect(() => {
-		if (!visible) return;
 		if (!dragRef.current) return;
-		if (!imgRef.current) return;
-		// 初始化图片对象
-		initImage();
-		const dragElement = dragRef.current;
-		const imageElement = imgRef.current;
-		dragElement.onmousedown = (e) => {
-			// 记录点击距离页面坐标
-			const pageX = e.pageX;
-			const pageY = e.pageY;
-			// 记录左边和上边的距离
-			const left = dragElement.offsetLeft;
-			const top = dragElement.offsetTop;
-			e.preventDefault();
-			document.onmousemove = (ev) => {
-				let curT = ev.pageY - pageY + top;
-				let curL = ev.pageX - pageX + left;
-				// 边界处理
-				const minL = 0;
-				const minT = 0;
-				const maxL = imageElement.clientWidth - dragElement.offsetWidth;
-				const maxT = imageElement.clientHeight - dragElement.offsetHeight;
-				curL = curL < minL ? minL : curL > maxL ? maxL : curL;
-				curT = curT < minT ? minT : curT > maxT ? maxT : curT;
-				dragElement.style.left = curL + 'px';
-				dragElement.style.top = curT + 'px';
-				console.log(curL, curT);
-				clipImage(
-					curL,
-					curT,
-					dragElement.clientWidth,
-					dragElement.clientHeight
-				);
-			};
-		};
-		document.onmouseup = () => {
-			document.onmousemove = null;
-		};
-	}, [visible]);
+		if (!wrapRef.current) return;
+		if (!canvasRef.current) return;
+		initCanvas();
+	}, []);
 
 	return (
-		<div>
-			<Dialog
-				visible={visible}
-				changeVisible={setVisible}
-				width="large"
-				title="编辑图片"
-			>
-				<div className={styles['container']}>
-					<div className={styles['tail_area']}>
-						<div className={styles['drag_crop']} ref={dragRef}>
-							<div className={styles['drag_crop_point_t']}></div>
-							<div className={styles['drag_crop_point_b']}></div>
-						</div>
-						<img ref={imgRef} className={styles['img']} src={src} alt="" />
-						{/* 	<canvas ref={canvasRef} /> */}
-					</div>
-					<div className={styles['tail_result']}>
-						<img src={base64URL} alt="" className={styles['tail_result_img']} />
-						<span className={styles['tail_result_text']}>预览</span>
-					</div>
+		<div className={styles['container']}>
+			<div className={styles['tail_area']} ref={wrapRef}>
+				<div className={styles['drag_crop']} ref={dragRef}>
+					<div className={styles['drag_crop_point_t']}></div>
+					<div className={styles['drag_crop_point_b']}></div>
 				</div>
-			</Dialog>
+				<canvas ref={canvasRef} />
+			</div>
+			<div className={styles['tail_result']}>
+				{/* <img src={base64URL} alt="" className={styles['tail_result_img']} /> */}
+				<span className={styles['tail_result_text']}>预览</span>
+			</div>
 		</div>
 	);
 };
