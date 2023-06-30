fb8aa32cb2a90d8a43885813fbcdb06d60226ec1
‘muddyrain’ <qiu3291002845@gmail.com>
2023-07-01 01:53:48 +0800

    chore(dumirc): 修复dumirc base 路由地址异常问题

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
 	logo: '/logo.png',
 	outputPath: 'docs-dist',
-	base: '.',
 	runtimePublicPath: {},
 	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	themeConfig: {
