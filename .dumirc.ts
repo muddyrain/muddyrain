c95ef220622ece795d2b27a49d46f1b1feb79a08
‘muddyrain’ <qiu3291002845@gmail.com>
2023-07-01 01:47:17 +0800

    fix(github actions): 修复 docs-dist 静态资源相对路径问题

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
 	logo: '/logo.png',
 	outputPath: 'docs-dist',
+	base: '.',
+	runtimePublicPath: {},
+	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	themeConfig: {
 		name: 'muddyrain',
 	},
