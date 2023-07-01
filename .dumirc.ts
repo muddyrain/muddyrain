054c6a2f7d3cf831f27dcb91bbf871a2e73e90a2
‘muddyrain’ <qiu3291002845@gmail.com>
2023-07-01 10:58:14 +0800

    chore(github actions): github pages 编译调试

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,8 +4,12 @@ export default defineConfig({
 	logo: '/logo.png',
 	outputPath: 'docs-dist',
 	runtimePublicPath: {},
-	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/muddyrain' : '/',
 	themeConfig: {
 		name: 'muddyrain',
+		socialLinks: {
+			github: 'https://github.com/muddyrain/muddyrain',
+			yuque: 'https://www.yuque.com/muddyrain',
+		},
 	},
 });
