fc82669d4354241b57962f85e806674c55e21545
‘muddyrain’ <qiu3291002845@gmail.com>
2023-07-01 12:46:13 +0800

    chore(github actions): 调试 v0.0.8

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,17 +5,25 @@ const defaultPath = '/muddyrain';
 //打包后gh-pages默认会拼接仓库名称在路径上
 const baseUrl = process.env.NODE_ENV === 'production' ? defaultPath : '';
 
-console.log(`${baseUrl}/`);
+const deployConfig =
+	process.env.NODE_ENV === 'production'
+		? {
+				base: '/muddyrain/',
+				publicPath: '/muddyrain/',
+				logo: '/muddyrain/logo.png',
+		  }
+		: {
+				base: '/',
+				publicPath: '/',
+				logo: '/logo.png',
+		  };
 export default defineConfig({
-	logo: '/logo.png',
+	...deployConfig,
 	outputPath: 'docs-dist',
-	base: '/muddyrain/',
-	publicPath: '/muddyrain/',
-
-	// themeConfig: {
-	// 	socialLinks: {
-	// 		github: 'https://github.com/muddyrain/muddyrain',
-	// 		yuque: 'https://www.yuque.com/muddyrain',
-	// 	},
-	// },
+	themeConfig: {
+		socialLinks: {
+			github: 'https://github.com/muddyrain/muddyrain',
+			yuque: 'https://www.yuque.com/muddyrain',
+		},
+	},
 });
